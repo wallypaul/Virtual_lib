fdec9d2cc3caf5acf8f67c4db3b06d8ddd37b482
Kagwe Allday <31845077+wallypaul@users.noreply.github.com>
2024-02-05 19:17:43 +0300

    Created books endpoint

--- app/Http/Controllers/Api/BooksController.php
+++ app/Http/Controllers/Api/BooksController.php
@@ -2,13 +2,27 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Books;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class BooksController extends Controller
 {
     //
     public function index() {
-        return 'BooksCOntrollers succefully contacted'
-    }
+        $books = Books::all();
+        if ($books->count() > 0) {
+            return response()->json([
+                'status' => 202,
+                'data' => $books
+                ], 200);
+        }
+        else {
+            return response()->json([
+                'status' => 404,
+                'message' => "No books found"
+                ], 404);
+        }
+                
+        }
 }
